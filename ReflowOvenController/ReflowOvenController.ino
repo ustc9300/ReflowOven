eda05ea36d3f7050d19f495bfe3fcf3d33b8534d
ustc9300 <ustc9300@gmail.com>
2016-06-27 22:21:17 +0800

    Initial verison.

---
+++ ReflowOvenController/ReflowOvenController.ino
@@ -0,0 +1,83 @@
+/*******************************************************************************
+* Title: Reflow Oven Controller
+* Version: 0.01
+* Date: 2012/06/27
+* Author: Yong GE 
+* 
+* Brief
+* =====
+* This is an example firmware for our Arduino compatible reflow oven controller. 
+* The reflow curve used in this firmware is meant for lead-free profile 
+* (it's even easier for leaded process!). You'll need to use the MAX6675
+* library for Arduino if you are having a shield of v1.60 & above which can be 
+* downloaded from our GitHub repository. Please check our wiki 
+* (www.rocketscream.com/wiki) for more information on using this piece of code 
+* together with the reflow oven controller shield. 
+*
+* Temperature (Degree Celcius)                 Magic Happens Here!
+* 245-|                                               x  x  
+*     |                                            x        x
+*     |                                         x              x
+*     |                                      x                    x
+* 200-|                                   x                          x
+*     |                              x    |                          |   x   
+*     |                         x         |                          |       x
+*     |                    x              |                          |
+* 150-|               x                   |                          |
+*     |             x |                   |                          |
+*     |           x   |                   |                          | 
+*     |         x     |                   |                          | 
+*     |       x       |                   |                          | 
+*     |     x         |                   |                          |
+*     |   x           |                   |                          |
+* 30 -| x             |                   |                          |
+*     |<  60 - 90 s  >|<    90 - 120 s   >|<       90 - 120 s       >|
+*     | Preheat Stage |   Soaking Stage   |       Reflow Stage       | Cool
+*  0  |_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+*                                                                Time (Seconds)
+*
+* This firmware owed very much on the works of other talented individuals as
+* follows:
+* ==========================================
+* Brett Beauregard (www.brettbeauregard.com)
+* ==========================================
+* Author of Arduino PID library. On top of providing industry standard PID 
+* implementation, he gave a lot of help in making this reflow oven controller 
+* possible using his awesome library.
+*
+* ==========================================
+* Limor Fried of Adafruit (www.adafruit.com)
+* ==========================================
+* Author of Arduino MAX6675 library. Adafruit has been the source of tonnes of
+* tutorials, examples, and libraries for everyone to learn.
+*
+* Disclaimer
+* ==========
+* Dealing with high voltage is a very dangerous act! Please make sure you know
+* what you are dealing with and have proper knowledge before hand. Your use of 
+* any information or materials on this reflow oven controller is entirely at 
+* your own risk, for which we shall not be liable. 
+*
+* Licences
+* ========
+* This reflow oven controller hardware and firmware are released under the 
+* Creative Commons Share Alike v3.0 license
+* http://creativecommons.org/licenses/by-sa/3.0/ 
+* You are free to take this piece of code, use it and modify it. 
+* All we ask is attribution including the supporting libraries used in this 
+* firmware. 
+*
+* Required Libraries
+* ==================
+* - Arduino PID Library: 
+*   >> https://github.com/br3ttb/Arduino-PID-Library
+* - MAX31855 Library (for board v1.60 & above): 
+*   >> https://github.com/rocketscream/MAX31855
+* - MAX6675 Library (for board v1.50 & below):
+*   >> https://github.com/adafruit/MAX6675-library
+*
+* Revision  Description
+* ========  ===========
+* 1.10      Arduino IDE 1.0 compatible.
+* 1.00      Initial public release.
+******************************************************************************
